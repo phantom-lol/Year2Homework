60e0b7c8245095a237d301be67b5d98ca162dc79
jacob <jacobtrippier@gmail.com>
2024-09-21 22:31:08 +0100

    Add project files.

---
+++ A200/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A200
+{
+    internal class Program
+    {
+        static List<int> alphabetPositions(List<string> strings)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            List<int> numbers = new List<int>();
+            foreach (string s in strings)
+            {
+                int amount = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i].ToString().ToLower() == alphabet[i].ToString())
+                    {
+                        amount++;
+                    }
+                }
+                numbers.Add(amount);
+            }
+            return numbers;
+        }
+        
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string>();
+            strings.Add("ABodE");
+            strings.Add("xBkd");
+            Console.WriteLine("Using words:");
+            foreach (string s in strings)
+            {
+                Console.WriteLine(s);
+            }
+            List<int> numbers = alphabetPositions(strings);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ A201/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A201
+{
+    internal class Program
+    {
+        static string FinalMap(string startMap)
+        {
+            string newMap = "";
+            bool infected = false;
+            int continentNum = 0;
+            foreach (char character in startMap)
+            {
+                if (character == 'X')
+                {
+
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Enter start map: ");
+            string map = Console.ReadLine();
+            int population = 0, infected = 0;
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                if (map[i] == '0')
+                {
+                    population++;
+                }
+                else if (map[i] == '1')
+                {
+                    infected++;
+                    population++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            double percent = (infected / population) * 100;
+            Console.WriteLine("Final Map: " + FinalMap(map));
+            Console.WriteLine("Total population: " + population);
+            Console.WriteLine("Number infected: " + infected);
+            Console.WriteLine($"Total Percentage of world infected is: {percent}%");
+            Console.ReadKey();
+        }
+    }
+}
