e65e9a8e3891b75042a4db1b041234e552e6a2ef
jacob <jacobtrippier@gmail.com>
2024-09-25 19:12:24 +0100

    A202 bubble sort

---
+++ A202/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A202
+{
+    internal class Program
+    {
+        static void bubbleSort(int[] array)
+        {
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] A = { 93, 46, 85, 90, 66, 57, 75, 5, 33, 45 };
+            bubbleSort(A);
+        }
+    }
+}
