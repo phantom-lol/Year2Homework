7c066b24ac6c7b8b47b4802d3e74838d2503d4e8
jacob <jacobtrippier@gmail.com>
2024-10-21 21:48:22 +0100

    A205 finished solution (used a hashset how fancy)

---
+++ A203/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A203
+{
+    internal class Program
+    {
+        static bool binarySearch(int[] array, int search)
+        {
+            int rear, front;
+            rear = 0;
+            front = array.Length - 1;
+
+            while (rear <= front)
+            {
+                int mid = (front + rear) / 2;
+
+                if (array[mid] == search)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (array[mid] < search)
+                    {
+                        rear = mid + 1;
+                    }
+                    else
+                    {
+                        front = mid - 1;
+                    }
+                }
+            }
+
+            return false;
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] a = { 2, 6, 14, 24, 39, 57, 63, 88 };
+
+            //if (binarySearch(a, 63) == true)
+            //{
+            //    Console.WriteLine("found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not found");
+            //}
+
+            if (binarySearch(a, 65) == true)
+            {
+                Console.WriteLine("found");
+            }
+            else
+            {
+                Console.WriteLine("not found");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ A205/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A205
+{
+    internal class Program
+    {
+        static bool HappyNumberTest(int num)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+            while (num != 1 && !numbers.Contains(num))
+            {
+                numbers.Add(num);
+                int sum = 0;
+                while (num > 0)
+                {
+                    int digit = num % 10;
+                    sum += digit * digit;
+                    num = num / 10;
+                }
+
+                num = sum;
+            }
+            
+            if (num == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(HappyNumberTest(a));
+            Console.ReadKey();
+        }
+    }
+}
