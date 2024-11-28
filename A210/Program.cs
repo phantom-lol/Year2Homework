9c789063b14f4a2d4fa1af9c780c59ddbc9f6c92
jacob <jacobtrippier@gmail.com>
2024-11-28 09:54:46 +0000

    a210 sigma

---
+++ A210/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A210
+{
+    internal class Program
+    {
+        static int GrawpOs(string text)
+        {
+            char[] oneO = { 'a', 'b', 'd', 'e', 'g', 'o', 'p', 'q', '0', '6', '9', 'D', 'O', 'P', 'Q', 'R', '@', '(', ')'};
+            char[] twoO = { '%', '&', 'B', '8' };
+            int numOfOs = 0;
+            foreach (char letter in text)
+            {
+                if (oneO.Contains(letter))
+                {
+                    numOfOs++;
+                }
+                else if (twoO.Contains(letter))
+                {
+                    numOfOs += 2;
+                }
+            }
+
+            return numOfOs;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text:");
+            string text = Console.ReadLine();
+            Console.WriteLine(GrawpOs(text));
+            Console.ReadKey();
+        }
+    }
+}
