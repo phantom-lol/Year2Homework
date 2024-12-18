d88146c45292769f889669dd704839031f6c03bd
jacob <jacobtrippier@gmail.com>
2024-12-18 14:52:53 +0000

    weird lights (not required btw)

--- A213/Program.cs
+++ A213/Program.cs
@@ -8,7 +8,25 @@ namespace A213
 {
     internal class Program
     {
-        
+        // idk kinda gives christmas lights but not really 
+        static ConsoleColor RandomColour()
+        {
+            Random rnd = new Random();
+            switch (rnd.Next(1,4))
+            {
+                case 1:
+                    return ConsoleColor.Red;
+                case 2:
+                    return ConsoleColor.Green;
+                case 3:
+                    return ConsoleColor.Cyan;
+                case 4:
+                    return ConsoleColor.Magenta;
+            }
+
+            return ConsoleColor.Yellow;
+        }
+
         static void CreateTree(int num)
         {
             int numOfLevels = num / 3;
@@ -23,15 +41,18 @@ namespace A213
             int carriedNum = 0;
             for (int i = 0; i < numOfLevels; i++)
             {
-                
                 while (row <= 3)
                 {
                     Console.CursorLeft = maxStars - (numOfStars / 2);
                     for (int j = 0; j < numOfStars - 1; j++)
                     {
+                        Console.ForegroundColor = RandomColour(); 
                         Console.Write("*");
+                        Console.ResetColor();
                     }
+                    Console.ForegroundColor = RandomColour();
                     Console.WriteLine("*");
+                    Console.ResetColor();
 
                     if (row == 2)
                     {
@@ -43,7 +64,6 @@ namespace A213
 
                 row = 1;
                 numOfStars -= 4;
-                Console.ResetColor();
             }
 
             Console.CursorLeft = maxStars - 1;
